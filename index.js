a4e1eac70678f1347ec9a91fea461a63fa72a68b
george kinyanjui1 <george.kinyajui@student.moringaschool.com>
2022-06-16 05:24:52 +0300

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-// Write your code here!
-const main = document.querySelector("main");
-main.remove();
-const newHeader = document.createElement('h1')
-newHeader.id = "victory"; //or the code below
-//newHeader.setAttribute("id", "victory");
+// // Write your code here!
+// const main = document.querySelector("main");
+// main.remove();
+// const newHeader = document.createElement('h1')
+// newHeader.id = "victory"; //or the code below
+// //newHeader.setAttribute("id", "victory");
+// newHeader.innerHTML = "George Mwicigi is the champion";main.remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
 newHeader.innerHTML = "George Mwicigi is the champion";
+main.remove()
