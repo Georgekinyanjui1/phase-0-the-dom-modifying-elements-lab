bf89856af678fde856fd1bc655f4c17f67152710
george kinyanjui1 <george.kinyajui@student.moringaschool.com>
2022-06-16 05:21:09 +0300

    commit tree f07bdd143ce6166a17170034340cf00ed909ef07

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-// Write your code here!
-const main = document.querySelector("main");
-main.remove();
-const newHeader = document.createElement('h1')
-newHeader.id = "victory"; //or the code below
-//newHeader.setAttribute("id", "victory");
-newHeader.innerHTML = "George Mwicigi is the champion";
+// // Write your code here!
+// const main = document.querySelector("main");
+// main.remove();
+// const newHeader = document.createElement('h1')
+// newHeader.id = "victory"; //or the code below
+// //newHeader.setAttribute("id", "victory");
+// newHeader.innerHTML = "George Mwicigi is the champion";
+const newHeader = document.getElementById('victory')
